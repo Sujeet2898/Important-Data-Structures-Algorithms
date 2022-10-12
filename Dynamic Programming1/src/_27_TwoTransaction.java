dfac4be070ca9406ae141293c290dd0fb8e1a459
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-10-12 13:40:12 +0530

    Update _27_TwoTransaction.java

--- Dynamic Programming1/src/_27_TwoTransaction.java
+++ Dynamic Programming1/src/_27_TwoTransaction.java
@@ -5,7 +5,28 @@ Question: Buy And Sell Stocks - Two Transactions Allowed
 2. You are given n numbers, where ith number represents price of stock on ith day.
 3. You are required to print the maximum profit you can make if you are allowed two transactions at-most.
 Note - There can be no overlapping transaction. One transaction needs to be closed (a buy followed by a sell) before opening another transaction (another buy).
- */
+
+Leetcode same question
+----------------------
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete at most two transactions.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+*/
 
 import java.util.Scanner;
 

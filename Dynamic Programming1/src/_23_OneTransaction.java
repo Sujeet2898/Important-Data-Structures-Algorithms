fa48f63904bbf3bee4eb1011ffde7a9524a0395a
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-10-12 13:33:54 +0530

    Update _23_OneTransaction.java

--- Dynamic Programming1/src/_23_OneTransaction.java
+++ Dynamic Programming1/src/_23_OneTransaction.java
@@ -4,6 +4,21 @@ Question: Buy And Sell Stocks - One Transaction Allowed
 1. You are given a number n, representing the number of days.
 2. You are given n numbers, where ith number represents price of stock on ith day.
 3. You are required to print the maximum profit you can make if you are allowed a single transaction.
+
+Leetcode same Question
+----------------------
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
  */
 
 import java.util.Scanner;

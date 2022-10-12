62d7a9740da36755ece9d0e29042398a86a51165
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-10-12 13:49:59 +0530

    Update _26_WithCoolDownInfiniteTransactions.java

--- Dynamic Programming1/src/_26_WithCoolDownInfiniteTransactions.java
+++ Dynamic Programming1/src/_26_WithCoolDownInfiniteTransactions.java
@@ -6,6 +6,21 @@ Question: Buy And Sell Stocks With Cooldown - Infinite Transaction Allowed
 3. You are required to print the maximum profit you can make if you are allowed infinite transactions, but have to cooldown for 1 day after 1 transaction
 i.e. you cannot buy on the next day after you sell, you have to cooldown for a day at-least before buying again.
 Note - There can be no overlapping transaction. One transaction needs to be closed (a buy followed by a sell) before opening another transaction (another buy).
+ 
+ Leetcode same question
+ ----------------------
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+Input: prices = [1]
+Output: 0
+
  */
 
 import java.util.Scanner;

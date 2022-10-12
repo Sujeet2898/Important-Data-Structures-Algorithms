5e73d774e78f9269a89355be4d77509063b4b8d7
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-10-12 13:37:02 +0530

    Update _24_InfiniteTransactions.java

--- Dynamic Programming1/src/_24_InfiniteTransactions.java
+++ Dynamic Programming1/src/_24_InfiniteTransactions.java
@@ -4,7 +4,25 @@ Question: Buy And Sell Stocks - Infinite Transactions Allowed
 
 You have been given stock values/prices for N number of days. Every i-th day signifies the price of a stock on that day. Your task is to find the maximum profit which you can make by buying and selling the stocks.
 Note: You may make as many transactions as you want but can not have more than one transaction at a time i.e, if you have the stock, you need to sell it first, and then only you can buy it again.
- */
+
+Leetcode same Question
+----------------------
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4
+
+*/
 
 import java.util.Scanner;
 
